44af52c626e46e021126e5010e1629ad989e427e
chiadikaobixo <chiadikaobixo@gmail.com>
2022-11-22 15:35:43 -0600

    minor update

--- contracts/Chiadi.sol
+++ contracts/Chiadi.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 
 contract Chiadi is IERC20 {
